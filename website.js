8bc6b769996f914220f3728e187b75ba1ecce74d
Yaseen Khan <kyaseen49@yahoo.com>
2023-09-17 16:41:51 -0700

    Added Scrolling Animations

---
+++ website.js
@@ -0,0 +1,31 @@
+// transition code for the hidden, show
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const getHiddenElems = document.querySelectorAll('.hidden');
+getHiddenElems.forEach((el) => observer.observe(el));
+
+// transition code for the complex and simple
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('complex');
+        }
+        else {
+            entry.target.classList.remove('complex');
+        }
+    });
+});
+
+const getHiddenElems2 = document.querySelectorAll('.simple');
+getHiddenElems2.forEach((el) => observer2.observe(el));
